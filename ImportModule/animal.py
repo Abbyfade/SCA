fe5d9b1b3f82aafe45b90945932ad58daa0c8537
abiola <60517156+Abbyfade@users.noreply.github.com>
2020-06-30 15:10:16 +0100

    Update animal.py

--- ImportModule/animal.py
+++ ImportModule/animal.py
@@ -1,5 +1,6 @@
-from sd1.cat import listBreeds
-from sd2.dog import main
-if __name__ == "__main__":
+from sd1.cat import listBreeds #import listBreeds from the cat.py module
+from sd2.dog import main #import main from the dog.py module
+
+if __name__ == "__main__": #excecuting the modules if invoked directly
     listBreeds()
     main()
