9245834be99e8a8881f4f1fa6c82eb1c59e4354b
abiola <60517156+Abbyfade@users.noreply.github.com>
2020-06-30 14:22:56 +0100

    added 7 files

---
+++ animal.py
@@ -0,0 +1,5 @@
+from sd1.cat import listBreeds
+from sd2.dog import main
+if __name__ == "__main__":
+    listBreeds()
+    main()
---
+++ sd2/dog.py
@@ -0,0 +1,7 @@
+from sd1.cat import breed
+
+cs = breed
+
+def main():
+    for c in cs:
+        print(c)
